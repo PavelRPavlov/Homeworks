9390a62271ab9705c9660b565c7650a933dc9bb8
Pavel Pavlov <pavel.raykov.pavlov@gmail.com>
2022-03-14 19:14:44 +0200

    Finalize homework 1 task 2

--- Homework2/Homework2.cpp
+++ Homework2/Homework2.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <math.h>
 #include "FileManager.h"
 #include "Homework2.h"
 #include "CustomColor.h"
@@ -8,31 +9,49 @@ using namespace std;
 
 int main()
 {
-	RunCreateImageTask();
-	RunCreateSingleShapeTask();
+	RunCreateImageTask(1920, 1080, 4, 4);
+	RunCreateSingleShapeTask(1920, 1080, 300);
 
 	return 1;
 }
 
-void RunCreateSingleShapeTask() {
-	// TODO implement second task from homework #2
-}
-
-void RunCreateImageTask() {
-	int imageWidth = 1920;
-	int imageHeight = 1080;
-	int imageColumns = 4;
-	int imageRows = 4;
-	int rectangleWidth = imageWidth / imageColumns;
-	int rectangleHeight = imageHeight / imageRows;
-	auto** bigPicture = new CustomColor * [imageHeight];
-	auto* singleRectangleColorMask = new CustomColor();
-	auto* drawer = new RectangleDrawer(bigPicture);
+void RunCreateSingleShapeTask(int imageWidth, int imageHeight, int circleRadius) {
+	int shapeOriginX = imageHeight / 2;
+	int shapeOriginY = imageWidth / 2;
+	CustomColor backgroundColor = CustomColor(255, 255, 255);
+	CustomColor foregroundColor = CustomColor(0, 0, 0);
+	auto** bigPicture = InitPicture(imageWidth, imageHeight);
+	double sqrCircleRadius = circleRadius * circleRadius;
 
 	for (int x = 0; x < imageHeight; x++)
 	{
-		bigPicture[x] = new CustomColor[imageWidth];
+		for (int y = 0; y < imageWidth; y++)
+		{
+			double dx = shapeOriginX - x;
+			double dy = shapeOriginY - y;
+
+			if (dx*dx + dy*dy <= sqrCircleRadius)
+			{
+				bigPicture[x][y] = foregroundColor;
+			}
+			else
+			{
+				bigPicture[x][y] = backgroundColor;
+			}
+		}
 	}
+
+	auto* fileManager = new FileManager(imageWidth, imageHeight);
+	const char* fileName = "ShapePicture.ppm";
+	fileManager->CreateFile(fileName, bigPicture);
+}
+
+void RunCreateImageTask(int imageWidth, int imageHeight, int imageColumns, int imageRows) {
+	int rectangleWidth = imageWidth / imageColumns;
+	int rectangleHeight = imageHeight / imageRows;
+	auto** bigPicture = InitPicture(imageWidth, imageHeight);
+	auto* singleRectangleColorMask = new CustomColor();
+	auto* drawer = new RectangleDrawer(bigPicture);
 	for (int x = 0; x < imageRows; x++)
 	{
 		for (int y = 0; y < imageColumns; y++)
@@ -50,4 +69,14 @@ void RunCreateImageTask() {
 	auto* fileManager = new FileManager(imageWidth, imageHeight);
 	const char* fileName = "BigPicture.ppm";
 	fileManager->CreateFile(fileName, bigPicture);
+}
+
+CustomColor** InitPicture(int imageWidth, int imageHeight) {
+
+	auto** bigPicture = new CustomColor * [imageHeight];
+	for (int x = 0; x < imageHeight; x++)
+	{
+		bigPicture[x] = new CustomColor[imageWidth];
+	}
+	return bigPicture;
 }
--- Homework2/Homework2.h
+++ Homework2/Homework2.h
@@ -1,8 +1,7 @@
 #pragma once
 #include "CustomColor.h"
 
-void RunCreateImageTask();
-void RunCreateSingleShapeTask();
+void RunCreateImageTask(int imageWidth, int imageHeight, int imageColumns, int imageRows);
+void RunCreateSingleShapeTask(int imageWidth, int imageHeight, int circleRadius);
 int main();
-CustomColor** CreateSingleRectangle(CustomColor& colorMask, int rectangleWidth, int rectangleHeight);
-void DrawRectangle(int originX, int originY, CustomColor** colorRectangle, int width, int height);
+CustomColor** InitPicture(int imageWidth, int imageHeight);
